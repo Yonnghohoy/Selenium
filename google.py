199e62fb34691e58868443da0d445e60900ba3c1
yonnghohoy <akcnl5216@gmail.com>
2021-08-12 12:55:55 +0900

    selenium

---
+++ google.py
@@ -0,0 +1,45 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import urllib.request
+driver = webdriver.Chrome()
+driver.get("https://www.google.co.kr/imghp?hl=ko&ogbl")
+elem = driver.find_element_by_name("q")
+elem.send_keys("조코딩")
+elem.send_keys(Keys.RETURN)
+
+SCROLL_PAUSE_SEC = 1
+
+# 스크롤 높이 가져옴
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    # 끝까지 스크롤 다운
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # 1초 대기
+    time.sleep(SCROLL_PAUSE_SEC)
+
+    # 스크롤 다운 후 스크롤 높이 다시 가져옴
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        try:
+            driver.find_element_by_css_selector(".mye4qd").click()
+        except: 
+            break
+
+    last_height = new_height
+
+images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+count = 1
+for image in images:
+    try:
+        image.click()
+        time.sleep(2)
+        imgUrl = driver.find_element_by_xpath("/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div[1]/a/img").get_attribute("src")
+        urllib.request.urlretrieve(imgUrl, str(count)+".jpg")
+        count = count +1
+    except:
+        pass
+
+driver.close()
